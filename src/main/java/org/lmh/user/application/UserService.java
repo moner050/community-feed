a093b51e446aed2b4787a06db88294e3e8b53556
moner050 <minhyung0237@gmail.com>
2025-01-03 00:44:57 +0900

    Feat: 유저 서비스 클래스 생성

---
+++ src/main/java/org/lmh/user/application/UserService.java
@@ -0,0 +1,28 @@
+package org.lmh.user.application;
+
+import org.lmh.user.application.dto.CreateUserRequestDto;
+import org.lmh.user.application.interfaces.UserRepository;
+import org.lmh.user.domain.User;
+import org.lmh.user.domain.UserInfo;
+
+import java.util.IllformedLocaleException;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(CreateUserRequestDto dto) {
+        UserInfo info = new UserInfo(dto.name(), dto.profileImageUrl());
+        User user = new User(null, info);
+
+        return userRepository.save(user);
+    }
+
+    public User getUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow(IllformedLocaleException::new);
+    }
+}
