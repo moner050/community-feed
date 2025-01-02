5fa0d9d65f0b4c899d74614c051513a200f37e00
moner050 <minhyung0237@gmail.com>
2025-01-02 19:15:15 +0900

    Feat: Content 추상화 메소드를 상속받아 게시글 유효성 체크를 하는 게시글 정보 VO 생성

---
+++ src/main/java/org/lmh/post/domain/content/PostContent.java
@@ -0,0 +1,22 @@
+package org.lmh.post.domain.content;
+
+public class PostContent extends Content {
+
+    private static final int MIN_POST_LENGTH = 5;
+    private static final int MAX_POST_LENGTH = 500;
+
+    public PostContent(String content) {
+        super(content);
+    }
+
+    @Override
+    protected void checkText(String contentText) {
+        if(contentText == null || contentText.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if(contentText.length() < MIN_POST_LENGTH || contentText.length() > MAX_POST_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
