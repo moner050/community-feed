2d3feb9da2e20bdaaa5730a2e440353bc5469b5b
moner050 <minhyung0237@gmail.com>
2025-01-02 19:14:26 +0900

    Feat: 게시글 및 댓글의 공통된 로직을 처리하는 추상화 클래스 생성

---
+++ src/main/java/org/lmh/post/domain/content/Content.java
@@ -0,0 +1,27 @@
+package org.lmh.post.domain.content;
+
+import org.lmh.post.domain.common.DateTimeInfo;
+
+public abstract class Content {
+
+    String contentText;
+    final DateTimeInfo dateTimeInfo;
+
+    protected Content(String contentText) {
+        checkText(contentText);
+        this.contentText = contentText;
+        this.dateTimeInfo = new DateTimeInfo();
+    }
+
+    public void updateContent(String updateContent) {
+        checkText(updateContent);
+        this.contentText = updateContent;
+        this.dateTimeInfo.updateEditDateTime();
+    }
+
+    protected abstract void checkText(String contentText);
+
+    public String getContentText() {
+        return contentText;
+    }
+}
