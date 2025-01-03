5da8a7765e1dfb58329716160578ca89e7352ff4
moner050 <minhyung0237@gmail.com>
2025-01-03 17:29:52 +0900

    Feat: 정적 생성자 추가

--- src/main/java/org/lmh/post/domain/comment/Comment.java
+++ src/main/java/org/lmh/post/domain/comment/Comment.java
@@ -2,6 +2,7 @@ package org.lmh.post.domain.comment;
 
 import org.lmh.common.domain.PositiveIntegerCounter;
 import org.lmh.post.domain.Post;
+import org.lmh.post.domain.content.CommentContent;
 import org.lmh.post.domain.content.Content;
 import org.lmh.user.domain.User;
 
@@ -12,6 +13,10 @@ public class Comment {
     private final User author;
     private final Content content;
     private final PositiveIntegerCounter likeCount;
+
+    public static Comment createComment(Post post, User author, String content) {
+        return new Comment(null, post, author, new CommentContent(content));
+    }
 
     public Comment(Long id, Post post, User author, Content content) {
         if(author == null) {
