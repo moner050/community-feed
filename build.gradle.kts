9fb43751c264a6cdb0ca1fd216491b5bc44a5d34
moner050 <minhyung0237@gmail.com>
2025-01-05 12:33:04 +0900

    Conf: Spring Boot 3.3.1 및 Spring Boot Web 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,28 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.lmh"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
