1ea1188f567c06ffb9e5b2a8585533c892adde69
moner050 <minhyung0237@gmail.com>
2025-01-05 21:33:19 +0900

    Feat: POJO 인 User 객체를 받는 UserEntity 생성

---
+++ src/main/java/org/lmh/user/repository/entity/UserEntity.java
@@ -0,0 +1,43 @@
+package org.lmh.user.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.lmh.common.domain.PositiveIntegerCounter;
+import org.lmh.common.repository.entity.TimeBaseEntity;
+import org.lmh.user.domain.User;
+import org.lmh.user.domain.UserInfo;
+
+@Entity
+@Table(name = "community_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UserEntity extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String profileImageUrl;
+    private Integer followerCount;
+    private Integer followingCount;
+
+    public UserEntity(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.profileImageUrl = user.getProfileImageUrl();
+        this.followerCount = user.followerCount();
+        this.followingCount = user.followingCount();
+    }
+
+    public User toUser() {
+        return User.builder()
+                .id(id)
+                .info(new UserInfo(name, profileImageUrl))
+                .followerCounter(new PositiveIntegerCounter(followerCount))
+                .followingCounter(new PositiveIntegerCounter(followingCount))
+                .build();
+    }
+}
