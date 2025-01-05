dbe547ca8ceeb68c56b644dfbffc80f7c39762d2
moner050 <minhyung0237@gmail.com>
2025-01-05 12:39:58 +0900

    Feat: HealthCheck 를 위한 컨트롤러 클래스 생성

---
+++ src/main/java/org/lmh/common/ui/HealthCheckController.java
@@ -0,0 +1,13 @@
+package org.lmh.common.ui;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
