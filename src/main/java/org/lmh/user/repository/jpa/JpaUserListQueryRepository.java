2887b93385a849ec103e38104ec9a7f1d83c68cc
moner050 <minhyung0237@gmail.com>
2025-01-07 00:24:07 +0900

    Feat: JPA 와 JPQL 을 사용해 DTO 에 맞는 값을 받아오는 레파지토리 생성

---
+++ src/main/java/org/lmh/user/repository/jpa/JpaUserListQueryRepository.java
@@ -0,0 +1,23 @@
+package org.lmh.user.repository.jpa;
+
+import org.lmh.user.application.dto.GetUserListResponseDto;
+import org.lmh.user.repository.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface JpaUserListQueryRepository extends JpaRepository<UserEntity, Long> {
+
+    @Query(value = "SELECT new org.lmh.user.application.dto.GetUserListResponseDto(u.name, u.profileImageUrl) " +
+            "FROM UserRelationEntity ur " +
+            "INNER JOIN UserEntity u ON ur.followingUserId = :userId " +
+            "WHERE ur.followingUserId = :userId")
+    List<GetUserListResponseDto> getFollowingUserList(Long userId);
+
+    @Query(value = "SELECT new org.lmh.user.application.dto.GetUserListResponseDto(u.name, u.profileImageUrl) " +
+            "FROM UserRelationEntity ur " +
+            "INNER JOIN UserEntity u ON ur.followingUserId = :userId " +
+            "WHERE ur.followerUserId = :userId")
+    List<GetUserListResponseDto> getFollowerUserList(Long userId);
+}
