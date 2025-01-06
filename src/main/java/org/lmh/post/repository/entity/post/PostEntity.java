cca8618e10dfce5cdba59160d3df079131791dd7
moner050 <minhyung0237@gmail.com>
2025-01-06 23:27:40 +0900

    Feat: POJO인 Post 객체를 생성자 매개변수로 받는 PostEntity 생성

---
+++ src/main/java/org/lmh/post/repository/entity/post/PostEntity.java
@@ -0,0 +1,54 @@
+package org.lmh.post.repository.entity.post;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.lmh.common.domain.PositiveIntegerCounter;
+import org.lmh.common.repository.entity.TimeBaseEntity;
+import org.lmh.post.domain.Post;
+import org.lmh.post.domain.content.PostContent;
+import org.lmh.post.domain.content.PostPublicationState;
+import org.lmh.user.repository.entity.UserEntity;
+
+@Entity
+@Table(name = "community_post")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class PostEntity extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // DDL 생성시, ForeignKey 생성 제한을 걸음.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT) )
+    private UserEntity author;
+
+    private String content;
+
+    @Convert(converter = PostPublicationStateConverter.class)
+    private PostPublicationState state;
+
+    private Integer likeCount;
+
+    public PostEntity(Post post) {
+        this.id = post.getId();
+        this.author = new UserEntity(post.getAuthor());
+        this.content = post.getContent();
+        this.state = post.getState();
+        this.likeCount = post.getLikeCount();
+    }
+
+    public Post toPost() {
+        return Post.builder()
+                .id(id)
+                .author(author.toUser())
+                .content(new PostContent(content))
+                .state(state)
+                .likeCount(new PositiveIntegerCounter(likeCount))
+                .build();
+    }
+}
