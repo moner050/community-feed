463c854cdb77424bac607742aaac74e49d89e313
moner050 <minhyung0237@gmail.com>
2025-01-08 17:52:25 +0900

    Refactor: findById 시 Optional 로 감싼 객체가 아닌 그냥 객체를 리턴하도록 수정

--- src/main/java/org/lmh/post/application/interfaces/PostRepository.java
+++ src/main/java/org/lmh/post/application/interfaces/PostRepository.java
@@ -2,10 +2,8 @@ package org.lmh.post.application.interfaces;
 
 import org.lmh.post.domain.Post;
 
-import java.util.Optional;
-
 public interface PostRepository {
 
     Post save(Post post);
-    Optional<Post> findById(Long id);
+    Post findById(Long id);
 }
