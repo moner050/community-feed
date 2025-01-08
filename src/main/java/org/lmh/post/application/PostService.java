a23fdc86cba5a66db8fa245e3299cac5f33c030f
moner050 <minhyung0237@gmail.com>
2025-01-08 17:54:56 +0900

    Refactor&Fix: findById 리턴타입 변경 및 DTO 생성방식 변경에 따른 getPost, updatePost 메소드 수정 / unlikePost 오류 수정

--- src/main/java/org/lmh/post/application/PostService.java
+++ src/main/java/org/lmh/post/application/PostService.java
@@ -1,27 +1,26 @@
 package org.lmh.post.application;
 
+import lombok.RequiredArgsConstructor;
 import org.lmh.post.application.dto.CreatePostRequestDto;
 import org.lmh.post.application.dto.LikeRequestDto;
+import org.lmh.post.application.dto.UpdatePostRequestDto;
 import org.lmh.post.application.interfaces.LikeRepository;
 import org.lmh.post.application.interfaces.PostRepository;
 import org.lmh.post.domain.Post;
 import org.lmh.user.application.UserService;
 import org.lmh.user.domain.User;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class PostService {
 
     private final UserService userService;
     private final PostRepository postRepository;
     private final LikeRepository likeRepository;
 
-    public PostService(UserService userService, PostRepository postRepository, LikeRepository likeRepository) {
-        this.userService = userService;
-        this.postRepository = postRepository;
-        this.likeRepository = likeRepository;
-    }
-
     public Post getPost(Long id) {
-        return postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Post not found"));
+        return postRepository.findById(id);
     }
 
     public Post createPost(CreatePostRequestDto dto) {
@@ -30,8 +29,8 @@ public class PostService {
         return postRepository.save(post);
     }
 
-    public Post updatePost(Long id, CreatePostRequestDto dto) {
-        Post post = getPost(id);
+    public Post updatePost(Long postId, UpdatePostRequestDto dto) {
+        Post post = getPost(postId);
         User user = userService.getUser(dto.userId());
 
         post.updatePost(user, dto.content(), dto.state());
@@ -57,7 +56,7 @@ public class PostService {
 
         if(likeRepository.checkLike(post, user)) {
             post.unlike();
-            likeRepository.like(post, user);
+            likeRepository.unlike(post, user);
         }
     }
 }
