7f736045a823366567597e0146e9d43f1dc94911
moner050 <minhyung0237@gmail.com>
2025-01-09 23:32:11 +0900

    Feat: 댓글 저장 시, 게시글의 댓글 수 늘리는 기능 추가

--- src/main/java/org/lmh/post/repository/CommentRepositoryImpl.java
+++ src/main/java/org/lmh/post/repository/CommentRepositoryImpl.java
@@ -3,9 +3,11 @@ package org.lmh.post.repository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.lmh.post.application.interfaces.CommentRepository;
+import org.lmh.post.domain.Post;
 import org.lmh.post.domain.comment.Comment;
 import org.lmh.post.repository.entity.comment.CommentEntity;
 import org.lmh.post.repository.jpa.JpaCommentRepository;
+import org.lmh.post.repository.jpa.JpaPostRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,10 +15,12 @@ import org.springframework.stereotype.Repository;
 public class CommentRepositoryImpl implements CommentRepository {
 
     private final JpaCommentRepository jpaCommentRepository;
+    private final JpaPostRepository jpaPostRepository;
 
     @Override
     @Transactional
     public Comment save(Comment comment) {
+        Post targetPost = comment.getPost();
         CommentEntity commentEntity = new CommentEntity(comment);
 
         // id 값이 없으면 UPDATE
@@ -26,6 +30,7 @@ public class CommentRepositoryImpl implements CommentRepository {
         }
 
         commentEntity = jpaCommentRepository.save(commentEntity);
+        jpaPostRepository.increaseCommentCount(targetPost.getId());
         return commentEntity.toComment();
     }
 
