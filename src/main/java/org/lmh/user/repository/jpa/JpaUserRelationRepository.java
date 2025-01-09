e08a9f880d599029920defda667566f0fd28f6f1
moner050 <minhyung0237@gmail.com>
2025-01-09 23:37:52 +0900

    Feat: 해당 유저의 팔로워들을 찾는 쿼리 메소드 생성

--- src/main/java/org/lmh/user/repository/jpa/JpaUserRelationRepository.java
+++ src/main/java/org/lmh/user/repository/jpa/JpaUserRelationRepository.java
@@ -3,6 +3,13 @@ package org.lmh.user.repository.jpa;
 import org.lmh.user.repository.entity.UserRelationEntity;
 import org.lmh.user.repository.entity.UserRelationIdEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JpaUserRelationRepository extends JpaRepository<UserRelationEntity, UserRelationIdEntity> {
+
+    @Query("SELECT u.followingUserId FROM UserRelationEntity u WHERE u.followerUserId = :userId")
+    List<Long> findFollowers(@Param("userId") Long userId);
 }
