94ee4bd04ec3bb3678778a2a4e5fd501411193ab
moner050 <minhyung0237@gmail.com>
2025-01-14 00:52:16 +0900

    Fix: 직렬화/역직렬화 할 수 있도록 각 날짜에 애너테이션 추가

--- src/main/java/org/lmh/common/repository/entity/TimeBaseEntity.java
+++ src/main/java/org/lmh/common/repository/entity/TimeBaseEntity.java
@@ -1,5 +1,10 @@
 package org.lmh.common.repository.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -17,8 +22,14 @@ public class TimeBaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime regDt;
 
     @LastModifiedDate
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime updDt;
 }
