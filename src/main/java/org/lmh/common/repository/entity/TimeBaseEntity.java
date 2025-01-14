ec1cd1c5bd9807d6a2905e4d54f599eb8acc8d40
moner050 <minhyung0237@gmail.com>
2025-01-14 15:59:21 +0900

    Feat: 저장일 및 수정일 시간 포멧을 yyyy-MM-dd HH:mm:ss 로 저장하도록 변경

--- src/main/java/org/lmh/common/repository/entity/TimeBaseEntity.java
+++ src/main/java/org/lmh/common/repository/entity/TimeBaseEntity.java
@@ -1,19 +1,13 @@
 package org.lmh.common.repository.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
@@ -22,14 +16,19 @@ public class TimeBaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime regDt;
 
     @LastModifiedDate
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime updDt;
+
+    @PrePersist
+    public void onPrePersist(){
+        this.regDt = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        this.updDt = this.regDt;
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.regDt = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
 }
