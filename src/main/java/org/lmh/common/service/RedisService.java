17eeab8f74ef10771a2f771f0ac4fba9723e25ea
moner050 <minhyung0237@gmail.com>
2025-01-14 15:37:21 +0900

    Feat: PK 값인 Long 타입 받을 예정이라 제너릭 타입 제거

--- src/main/java/org/lmh/common/service/RedisService.java
+++ src/main/java/org/lmh/common/service/RedisService.java
@@ -3,13 +3,13 @@ package org.lmh.common.service;
 import java.util.List;
 import java.util.Set;
 
-public interface RedisService<T> {
-    Set<T> getData(String key, Class<T> clazz);
-    void setData(String key, T value);
-    void setAllData(String key, List<T> values);
+public interface RedisService {
+    Set<Long> getData(String key);
+    void setData(String key, Long value);
+    void setAllData(String key, List<Long> values);
     void deleteData(String key);
-    void deleteValue(String key, T value);
+    void deleteValue(String key, Long value);
     void deleteValueByTargetId(String key, String targetId);
     boolean containsKey(String key);
-    boolean containsValue(String key, T value);
+    boolean containsValue(String key, Long value);
 }
