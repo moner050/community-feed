4d147fe6f2f8b2d6982e159515e902ae92073e38
moner050 <minhyung0237@gmail.com>
2025-01-15 23:32:52 +0900

    Feat: 생성일 및 업데이트일 전처리 로직 제거

--- src/main/java/org/lmh/common/repository/entity/TimeBaseEntity.java
+++ src/main/java/org/lmh/common/repository/entity/TimeBaseEntity.java
@@ -1,13 +1,14 @@
 package org.lmh.common.repository.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
@@ -20,15 +21,4 @@ public class TimeBaseEntity {
 
     @LastModifiedDate
     private LocalDateTime updDt;
-
-    @PrePersist
-    public void onPrePersist(){
-        this.regDt = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        this.updDt = this.regDt;
-    }
-
-    @PreUpdate
-    public void onPreUpdate() {
-        this.regDt = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-    }
 }
