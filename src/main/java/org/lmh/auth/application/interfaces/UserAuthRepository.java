09f51c988aeb39ff8af81ac3dd4d6aa52f0cbe9f
moner050 <minhyung0237@gmail.com>
2025-01-21 01:26:24 +0900

    Feat: 유저인증 관련 인터페이스 레파지토리 생성

---
+++ src/main/java/org/lmh/auth/application/interfaces/UserAuthRepository.java
@@ -0,0 +1,8 @@
+package org.lmh.auth.application.interfaces;
+
+import org.lmh.auth.domain.UserAuth;
+import org.lmh.user.domain.User;
+
+public interface UserAuthRepository {
+    UserAuth registerUser(UserAuth auth, User user);
+}
