97a688c9f653a47e1a6c262ceaba12820ddd0a89
moner050 <minhyung0237@gmail.com>
2025-01-20 23:49:04 +0900

    Test: 테스트 데이터 로딩 시 이메일 토큰 검색을 위해 EntityManager 를 이용한 네이티브 쿼리 조회 메소드 생성

--- src/test/java/org/lmh/acceptance/utils/DataLoader.java
+++ src/test/java/org/lmh/acceptance/utils/DataLoader.java
@@ -1,5 +1,7 @@
 package org.lmh.acceptance.utils;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.lmh.user.application.dto.CreateUserRequestDto;
 import org.lmh.user.application.dto.FollowUserRequestDto;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,9 @@ import static org.lmh.acceptance.steps.UserAcceptanceSteps.followUser;
 @Component
 public class DataLoader {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public void loadData() {
         CreateUserRequestDto dto = new CreateUserRequestDto("testUser", "");
         createUser(dto);        // 1번 유저 생성
@@ -18,5 +23,12 @@ public class DataLoader {
 
         followUser(new FollowUserRequestDto(1L, 2L));
         followUser(new FollowUserRequestDto(1L, 3L));
+    }
+
+    public String getEmailToken(String email) {
+        return entityManager.createNativeQuery("SELECT token FROM community_email_verification WHERE email = ?", String.class)
+                .setParameter(1, email)
+                .getSingleResult()
+                .toString();
     }
 }
