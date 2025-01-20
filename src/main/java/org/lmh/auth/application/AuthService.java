68d287419f4027ce80115c6530238510e43160a1
moner050 <minhyung0237@gmail.com>
2025-01-21 01:27:23 +0900

    Feat: 인증 관련 서비스 클래스 생성

---
+++ src/main/java/org/lmh/auth/application/AuthService.java
@@ -0,0 +1,33 @@
+package org.lmh.auth.application;
+
+import lombok.RequiredArgsConstructor;
+import org.lmh.auth.application.dto.CreateUserAuthRequestDto;
+import org.lmh.auth.application.interfaces.EmailVerificationRepository;
+import org.lmh.auth.application.interfaces.UserAuthRepository;
+import org.lmh.auth.domain.Email;
+import org.lmh.auth.domain.UserAuth;
+import org.lmh.user.domain.User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserAuthRepository userAuthRepository;
+    private final EmailVerificationRepository verificationRepository;
+
+    public Long registerUser(CreateUserAuthRequestDto dto) {
+        Email email = Email.createEmail(dto.email());
+
+        if(!verificationRepository.isEmailVerified(email)) {
+            throw new IllegalArgumentException("인증되지 않은 이메일입니다.");
+        }
+
+        UserAuth userAuth = new UserAuth(dto.email(), dto.password(), dto.role());
+        User user = new User(dto.name(), dto.profileImageUrl());
+        userAuth = userAuthRepository.registerUser(userAuth, user);
+
+        return userAuth.getUserId();
+    }
+
+}
