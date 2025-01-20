abb0acdeff4c80d722d210b34bee8f31f5052bfc
moner050 <minhyung0237@gmail.com>
2025-01-21 01:24:10 +0900

    Feat: 이메일 인증처리 및 이메일 인증 확인 인터페이스 메소드 생성

--- src/main/java/org/lmh/auth/application/interfaces/EmailVerificationRepository.java
+++ src/main/java/org/lmh/auth/application/interfaces/EmailVerificationRepository.java
@@ -4,4 +4,6 @@ import org.lmh.auth.domain.Email;
 
 public interface EmailVerificationRepository {
     void createEmailVerification(Email email, String token);
+    void verifyEmail(Email email, String token);
+    boolean isEmailVerified(Email email);
 }
