0c899a64c82708a8a29dd6b3d66cda4aa920f04b
moner050 <minhyung0237@gmail.com>
2025-01-21 01:17:49 +0900

    Feat: 이메일 인증 확인 및 회원가입 컨트롤러 메소드 생성

--- src/main/java/org/lmh/auth/ui/SignUpController.java
+++ src/main/java/org/lmh/auth/ui/SignUpController.java
@@ -1,13 +1,12 @@
 package org.lmh.auth.ui;
 
 import lombok.RequiredArgsConstructor;
+import org.lmh.auth.application.AuthService;
 import org.lmh.auth.application.EmailService;
+import org.lmh.auth.application.dto.CreateUserAuthRequestDto;
 import org.lmh.auth.application.dto.SendEmailRequestDto;
 import org.lmh.common.ui.Response;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/signup")
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignUpController {
 
     private final EmailService emailService;
+    private final AuthService authService;
 
     @PostMapping("/send-verification-email")
     public Response<Void> sendEmail(@RequestBody SendEmailRequestDto dto) {
@@ -22,4 +22,14 @@ public class SignUpController {
         return Response.ok(null);
     }
 
+    @GetMapping("/verify-token")
+    public Response<Void> verifyEmail(@RequestParam("email") String email, @RequestParam("token") String token) {
+        emailService.verifyEmail(email, token);
+        return Response.ok(null);
+    }
+
+    @PostMapping("/register")
+    public Response<Long> register(@RequestBody CreateUserAuthRequestDto dto) {
+        return Response.ok(authService.registerUser(dto));
+    }
 }
