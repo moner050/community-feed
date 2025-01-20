9619afcd4937febb7ff76301910b52b6c14d0015
moner050 <minhyung0237@gmail.com>
2025-01-21 01:19:02 +0900

    Feat: 이메일 인증 관련 토큰 비교 메소드 생성

--- src/main/java/org/lmh/auth/repository/entity/EmailVerificationEntity.java
+++ src/main/java/org/lmh/auth/repository/entity/EmailVerificationEntity.java
@@ -37,4 +37,8 @@ public class EmailVerificationEntity extends TimeBaseEntity {
     public void verify() {
         this.isVerified = true;
     }
+
+    public boolean hasSameToken(String token) {
+        return this.token.equals(token);
+    }
 }
