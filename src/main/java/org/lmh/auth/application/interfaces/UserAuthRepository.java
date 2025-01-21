064006aaf78e10394a844229fd94733d3fefe5c0
moner050 <minhyung0237@gmail.com>
2025-01-21 23:35:20 +0900

    Feat: 로그인 유저 정보 받아오는 인터페이스 메소드 생성 및 해당 메소드 구현

--- src/main/java/org/lmh/auth/application/interfaces/UserAuthRepository.java
+++ src/main/java/org/lmh/auth/application/interfaces/UserAuthRepository.java
@@ -5,4 +5,5 @@ import org.lmh.user.domain.User;
 
 public interface UserAuthRepository {
     UserAuth registerUser(UserAuth auth, User user);
+    UserAuth loginUser(String email, String password);
 }
--- src/main/java/org/lmh/auth/repository/UserAuthRepositoryImpl.java
+++ src/main/java/org/lmh/auth/repository/UserAuthRepositoryImpl.java
@@ -24,4 +24,16 @@ public class UserAuthRepositoryImpl implements UserAuthRepository {
 
         return userAuthEntity.toUserAuth();
     }
+
+    @Override
+    public UserAuth loginUser(String email, String password) {
+        UserAuthEntity entity = jpaUserAuthRepository.findById(email).orElseThrow();
+        UserAuth userAuth = entity.toUserAuth();
+
+        if(!userAuth.matchPassword(password)) {
+            throw new IllegalArgumentException("옳지 않은 비밀번호 입니다.");
+        }
+
+        return userAuth;
+    }
 }
