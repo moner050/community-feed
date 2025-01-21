542db26c368d4e5ab42b4d7bbf88d1edafbbe5c7
moner050 <minhyung0237@gmail.com>
2025-01-21 23:57:29 +0900

    Feat: 게시글 피드를 받아올 때, 유저 아이디가 아닌 토큰값을 이용해 받아오도록 수정

--- src/main/java/org/lmh/post/ui/FeedController.java
+++ src/main/java/org/lmh/post/ui/FeedController.java
@@ -1,11 +1,12 @@
 package org.lmh.post.ui;
 
 import lombok.RequiredArgsConstructor;
+import org.lmh.common.principal.AuthPrincipal;
+import org.lmh.common.principal.UserPrincipal;
 import org.lmh.common.ui.Response;
 import org.lmh.post.repository.post_queue.UserPostQueueQueryRepository;
 import org.lmh.post.ui.dto.GetPostContentResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +19,10 @@ public class FeedController {
 
     private final UserPostQueueQueryRepository queueQueryRepository;
 
-    @GetMapping("/{userId}")
-    public Response<List<GetPostContentResponseDto>> getPostFeed(@PathVariable Long userId, Long lastPostId) {
+    @GetMapping("")
+    public Response<List<GetPostContentResponseDto>> getPostFeed(@AuthPrincipal UserPrincipal userPrincipal, Long lastPostId) {
 
-        List<GetPostContentResponseDto> result = queueQueryRepository.getContentResponse(userId, lastPostId);
+        List<GetPostContentResponseDto> result = queueQueryRepository.getContentResponse(userPrincipal.getUserId(), lastPostId);
         return Response.ok(result);
     }
 }
