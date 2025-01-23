ea2e514e728f5f875a1be704eb0dcc114736f02e
moner050 <minhyung0237@gmail.com>
2025-01-23 23:54:13 +0900

    Feat: 이메일 전송 관련 JavaMailSender Bean 등록 하기 위한 Configuration 클래스 생성

---
+++ src/main/java/org/lmh/common/config/EmailConfig.java
@@ -0,0 +1,38 @@
+package org.lmh.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender mailSender(
+            @Value("${spring.mail.host}") String host,
+            @Value("${spring.mail.port}") int port,
+            @Value("${spring.mail.username}") String username,
+            @Value("${spring.mail.password}") String password,
+            @Value("${spring.mail.protocol}") String protocol,
+            @Value("${spring.mail.properties.mail.smtp.auth}") String auth,
+            @Value("${spring.mail.properties.mail.smtp.starttls.enable}") String starttls
+            ) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", protocol);
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", starttls);
+
+        return mailSender;
+    }
+}
