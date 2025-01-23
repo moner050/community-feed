72d9f7fc2a12e54b6f6508d82abc14813071cba0
moner050 <minhyung0237@gmail.com>
2025-01-23 23:12:45 +0900

    Build: 이메일 전송을 하기 위한 spring-boot-starter-mail 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
+    // 이메일
+    implementation ("org.springframework.boot:spring-boot-starter-mail")
+
     // Redis
     implementation ("org.springframework.boot:spring-boot-starter-data-redis")
 
