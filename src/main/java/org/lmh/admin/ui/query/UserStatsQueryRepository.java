02842cd4ae505a8906455472ecdfedf06d64d8d8
moner050 <minhyung0237@gmail.com>
2025-01-24 23:05:41 +0900

    Feat: 일별 유저 가입자 수 불러오는 레파지토리 생성

---
+++ src/main/java/org/lmh/admin/ui/query/UserStatsQueryRepository.java
@@ -0,0 +1,10 @@
+package org.lmh.admin.ui.query;
+
+import org.lmh.admin.ui.dto.GetDailyRegisterUserResponseDto;
+
+import java.util.List;
+
+public interface UserStatsQueryRepository {
+
+    List<GetDailyRegisterUserResponseDto> getDailyRegisterUserStats(int beforeDays);
+}
