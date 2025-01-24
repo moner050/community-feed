45150d79952b75763fcd2bb04de6bbd89061ceb2
moner050 <minhyung0237@gmail.com>
2025-01-25 02:26:05 +0900

    Feat: 유저 목록 페이지 관련 컨트롤러 메소드 추가

--- src/main/java/org/lmh/admin/ui/AdminController.java
+++ src/main/java/org/lmh/admin/ui/AdminController.java
@@ -1,6 +1,10 @@
 package org.lmh.admin.ui;
 
 import lombok.RequiredArgsConstructor;
+import org.lmh.admin.ui.dto.GetTableListResponse;
+import org.lmh.admin.ui.dto.users.GetUserTableRequestDto;
+import org.lmh.admin.ui.dto.users.GetUserTableResponseDto;
+import org.lmh.admin.ui.query.AdminTableQueryRepository;
 import org.lmh.admin.ui.query.UserStatsQueryRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +17,24 @@ import org.springframework.web.servlet.ModelAndView;
 public class AdminController {
 
     private final UserStatsQueryRepository userStatsQueryRepository;
+    private final AdminTableQueryRepository adminTableQueryRepository;
 
     @GetMapping("/index")
     public ModelAndView index() {
         ModelAndView mav = new ModelAndView("index");
 
         mav.addObject("result", userStatsQueryRepository.getDailyRegisterUserStats(7));
+        return mav;
+    }
+
+    @GetMapping("/users")
+    public ModelAndView users(GetUserTableRequestDto dto) {
+        ModelAndView mav = new ModelAndView("users");
+
+        GetTableListResponse<GetUserTableResponseDto> result = adminTableQueryRepository.getUserTableData(dto);
+        mav.addObject("requestDto", dto);
+        mav.addObject("userList", result.getTableData());
+        mav.addObject("totalCount", result.getTotalCount());
         return mav;
     }
 }
