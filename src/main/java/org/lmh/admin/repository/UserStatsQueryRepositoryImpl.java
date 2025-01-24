1c56b67838e365c7ffb8f1bb8f761a64a74c33f1
moner050 <minhyung0237@gmail.com>
2025-01-24 23:06:08 +0900

    Feat: 인터페이스 상속받아 QueryDSL 로 구현한 레파지토리 생성

---
+++ src/main/java/org/lmh/admin/repository/UserStatsQueryRepositoryImpl.java
@@ -0,0 +1,37 @@
+package org.lmh.admin.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.lmh.admin.ui.dto.GetDailyRegisterUserResponseDto;
+import org.lmh.admin.ui.query.UserStatsQueryRepository;
+import org.lmh.common.TimeCalculator;
+import org.lmh.user.repository.entity.QUserEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class UserStatsQueryRepositoryImpl implements UserStatsQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+    private static final QUserEntity userEntity = QUserEntity.userEntity;
+
+    @Override
+    public List<GetDailyRegisterUserResponseDto> getDailyRegisterUserStats(int beforeDays) {
+        return queryFactory
+                .select(
+                        Projections.fields(
+                                GetDailyRegisterUserResponseDto.class,
+                                userEntity.regDate.as("date"),
+                                userEntity.count().as("count")
+                        )
+                )
+                .from(userEntity)
+                .where(userEntity.regDate.after(TimeCalculator.getDateDaysAgo(beforeDays)))
+                .groupBy(userEntity.regDate)
+                .orderBy(userEntity.regDate.asc())
+                .fetch();
+    }
+}
