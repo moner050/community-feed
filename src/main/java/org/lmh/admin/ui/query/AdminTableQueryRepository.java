8697d8003214203a840077e449c0a8599c0bc569
moner050 <minhyung0237@gmail.com>
2025-02-03 23:16:12 +0900

    Feat: 게시글 데이터 가져오는 인터페이스 메소드 생성

--- src/main/java/org/lmh/admin/ui/query/AdminTableQueryRepository.java
+++ src/main/java/org/lmh/admin/ui/query/AdminTableQueryRepository.java
@@ -1,10 +1,13 @@
 package org.lmh.admin.ui.query;
 
 import org.lmh.admin.ui.dto.GetTableListResponse;
+import org.lmh.admin.ui.dto.post.GetPostTableRequestDto;
+import org.lmh.admin.ui.dto.post.GetPostTableResponseDto;
 import org.lmh.admin.ui.dto.users.GetUserTableRequestDto;
 import org.lmh.admin.ui.dto.users.GetUserTableResponseDto;
 
 public interface AdminTableQueryRepository {
 
     GetTableListResponse<GetUserTableResponseDto> getUserTableData(GetUserTableRequestDto dto);
+    GetTableListResponse<GetPostTableResponseDto> getPostTableData(GetPostTableRequestDto dto);
 }
