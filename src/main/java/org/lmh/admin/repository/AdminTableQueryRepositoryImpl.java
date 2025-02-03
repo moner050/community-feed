53c7d66da67e4b399593682827ad163d343a9db9
moner050 <minhyung0237@gmail.com>
2025-02-03 23:18:30 +0900

    Feat: queryDsl 과 커버링 인덱스를 이용해 유저 및 게시글 관련 데이터를 불러오는 메소드 수정 및 생성

--- src/main/java/org/lmh/admin/repository/AdminTableQueryRepositoryImpl.java
+++ src/main/java/org/lmh/admin/repository/AdminTableQueryRepositoryImpl.java
@@ -6,10 +6,13 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.lmh.admin.ui.dto.GetTableListResponse;
+import org.lmh.admin.ui.dto.post.GetPostTableRequestDto;
+import org.lmh.admin.ui.dto.post.GetPostTableResponseDto;
 import org.lmh.admin.ui.dto.users.GetUserTableRequestDto;
 import org.lmh.admin.ui.dto.users.GetUserTableResponseDto;
 import org.lmh.admin.ui.query.AdminTableQueryRepository;
 import org.lmh.auth.repository.entity.QUserAuthEntity;
+import org.lmh.post.repository.entity.post.QPostEntity;
 import org.lmh.user.repository.entity.QUserEntity;
 import org.springframework.stereotype.Repository;
 
@@ -21,6 +24,7 @@ public class AdminTableQueryRepositoryImpl implements AdminTableQueryRepository 
     private final JPAQueryFactory queryFactory;
     private static final QUserAuthEntity userAuthEntity = QUserAuthEntity.userAuthEntity;
     private static final QUserEntity userEntity = QUserEntity.userEntity;
+    private static final QPostEntity postEntity = QPostEntity.postEntity;
 
     @Override
     public GetTableListResponse<GetUserTableResponseDto> getUserTableData(GetUserTableRequestDto dto) {
@@ -31,6 +35,17 @@ public class AdminTableQueryRepositoryImpl implements AdminTableQueryRepository 
                 .where(likeName(dto.getName()))
                 .fetch()
                 .size();
+
+        List<Long> ids = queryFactory
+                .select(userEntity.id)
+                .from(userEntity)
+                .where(
+                        likeName(dto.getName())
+                )
+                .orderBy(userEntity.id.desc())
+                .offset(dto.getOffset())
+                .limit(dto.getLimit())
+                .fetch();
 
         List<GetUserTableResponseDto> result = queryFactory
                 .select(
@@ -47,10 +62,53 @@ public class AdminTableQueryRepositoryImpl implements AdminTableQueryRepository 
                 )
                 .from(userEntity)
                 .join(userAuthEntity).on(userAuthEntity.userId.eq(userEntity.id))
-                .where(likeName(dto.getName()))
+                .where(
+                        userEntity.id.in(ids)
+                )
                 .orderBy(userEntity.id.desc())
+                .fetch();
+
+        return new GetTableListResponse<>(total, result);
+    }
+
+    @Override
+    public GetTableListResponse<GetPostTableResponseDto> getPostTableData(GetPostTableRequestDto dto) {
+
+        int total = queryFactory
+                .select(postEntity.id)
+                .from(postEntity)
+                .where(eqPostId(dto.getPostId()))
+                .fetch().size();
+
+        List<Long> ids = queryFactory
+                .select(postEntity.id)
+                .from(postEntity)
+                .where(
+                        eqPostId(dto.getPostId())
+                )
+                .orderBy(postEntity.id.desc())
                 .offset(dto.getOffset())
                 .limit(dto.getLimit())
+                .fetch();
+
+        List<GetPostTableResponseDto> result = queryFactory
+                .select(
+                        Projections.fields(
+                                GetPostTableResponseDto.class,
+                                postEntity.id.as("postId"),
+                                userEntity.id.as("userId"),
+                                userEntity.name.as("userName"),
+                                postEntity.content.as("content"),
+                                postEntity.regDt.as("createdAt"),
+                                postEntity.updDt.as("updatedAt")
+                        )
+                )
+                .from(postEntity)
+                .join(userEntity).on(postEntity.author.id.eq(userEntity.id))
+                .where(
+                        postEntity.id.in(ids)
+                )
+                .orderBy(postEntity.id.desc())
                 .fetch();
 
         return new GetTableListResponse<>(total, result);
@@ -62,5 +120,13 @@ public class AdminTableQueryRepositoryImpl implements AdminTableQueryRepository 
             return null;
         }
         return userEntity.name.like(name + "%");
+    }
+
+    private BooleanExpression eqPostId(Long postId) {
+        if(postId == null) {
+            return null;
+        }
+
+        return postEntity.id.eq(postId);
     }
 }
