bd454cf56da1c6faec76f59ea2eda2908f6f0451
moner050 <minhyung0237@gmail.com>
2025-02-25 23:40:28 +0900

    Build: firebase-admin 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,9 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-gson:0.12.6")
 
+    // firebase
+    implementation("com.google.firebase:firebase-admin:9.4.3")
+
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
