15a5c2eec316f198da704f2594245b2047a25e7b
moner050 <minhyung0237@gmail.com>
2025-02-28 01:56:19 +0900

    Feat: JPA 를 이용해 Fcm token 관련 레파지토리 생성

---
+++ src/main/java/org/lmh/message/repository/JpaFcmTokenRepository.java
@@ -0,0 +1,8 @@
+package org.lmh.message.repository;
+
+import org.lmh.message.repository.entity.FcmTokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaFcmTokenRepository extends JpaRepository<FcmTokenEntity, Long> {
+
+}
